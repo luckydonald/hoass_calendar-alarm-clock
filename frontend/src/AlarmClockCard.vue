046aa5454d27aab2b0fb4094c3e2cfebba7c6bfb
luckydonald <m1-mac-2024._.code@luckydonald.de>
2026-01-30 04:16:52 +0100

    👩‍💻 Fixed error in vue room.

--- frontend/src/AlarmClockCard.vue
+++ frontend/src/AlarmClockCard.vue
@@ -1117,7 +1117,6 @@ function handleAddButtonClick(): void {
           <ha-icon :icon="isEditing ? 'mdi:pencil' : 'mdi:alarm-plus'" />
           <span>{{ isEditing ? 'Edit Alarm' : 'Add Alarm' }}</span>
         </div>
-      </template>
 
       <div class="dialog-content">
         <ha-textfield
