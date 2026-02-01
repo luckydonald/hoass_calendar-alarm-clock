6b72e8d0e2fa6ef2a9d6c19001117841198d8db1
luckydonald <m1-mac-2024._.code@luckydonald.de>
2026-02-02 00:48:57 +0100

    🔧📘 lint: eslint autofix

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -95,16 +95,14 @@ export interface Alarm extends AlarmAttributes {
   state: AlarmState;
 }
 
-export type AlarmState =
-  | 'before'
+export type AlarmState = | 'before'
   | 'ringing'
   | 'ringing_snooze'
   | 'snoozed'
   | 'dismissed'
   | 'timed_out';
 
-export type RepeatPattern =
-  | 'none'
+export type RepeatPattern = | 'none'
   | 'daily'
   | 'weekdays'
   | 'weekends'
