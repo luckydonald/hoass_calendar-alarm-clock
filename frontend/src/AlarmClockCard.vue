90677b726d82788b1233039987e1ead9697f7a72
luckydonald <m1-mac-2024._.code@luckydonald.de>
2026-02-02 02:14:10 +0100

    🔧📘 lint: eslint autofix

--- frontend/src/AlarmClockCard.vue
+++ frontend/src/AlarmClockCard.vue
@@ -299,8 +299,7 @@ const formattedDate = computed(() =>
     weekday: 'long',
     month: 'long',
     day: 'numeric',
-  })
-);
+  }));
 
 const pad = (v: number) => String(v).padStart(2, '0');
 const current12Hours = computed(() => ((currentHours.value + 11) % 12) + 1);
